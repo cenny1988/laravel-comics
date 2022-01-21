a515dd7807467510846f0d5aaab14f00fc269aea
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2022-01-21 15:59:22 +0100

    add header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,1 +1,24 @@
-<h1>DC COMICS</h1>
+<header>
+    <div class="container">
+        <div id="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo-dc">
+        </div>
+        <nav>
+            <ul>
+                <li><a  href="#">CHARACTERS</a></li>
+                <li class="active"><a  href="#">COMICS</a></li>
+                <li><a  href="#">MOVIES</a></li>
+                <li><a  href="#">TV</a></li>
+                <li><a  href="#">GAMES</a></li>
+                <li><a  href="#">CCOLLECTIBLES</a></li>
+                <li><a  href="#">VIDEOS</a></li>
+                <li><a  href="#">FANS</a></li>
+                <li><a  href="#">NEWS</a></li>
+                <li><a  href="#">SHOP</a></li>
+            </ul>
+            <!-- <a href="#" class="close">
+                  <i class="fas fa-times"></i>
+              </a> -->
+        </nav>
+    </div>
+</header>
